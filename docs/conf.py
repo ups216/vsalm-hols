ce3d777e8fb1cd54ab118e13553b4dd1402f0da7
Lei Xu <leixu216@gmail.com>
2016-02-10 20:46:13 +0800

    try

--- docs/conf.py
+++ docs/conf.py
@@ -184,11 +184,20 @@ latex_elements = {
 
 # Additional stuff for the LaTeX preamble.
 'preamble': '''
-\usepackage{xeCJK}
-\setCJKmainfont{WenQuanYi Zen Hei Sharp}
-\setCJKmonofont[Scale=0.9]{WenQuanYi Zen Hei Mono}
-\setCJKfamilyfont{song}{WenQuanYi Zen Hei}
-\setCJKfamilyfont{sf}{WenQuanYi Zen Hei}
+\hypersetup{unicode=true}
+\usepackage{CJKutf8}
+\DeclareUnicodeCharacter{00A0}{\nobreakspace}
+\DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
+\DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
+\DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
+\DeclareUnicodeCharacter{2713}{x}
+\DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
+\DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}}
+\DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
+\DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
+\DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
+\begin{CJK}{UTF8}{gbsn}
+\AtEndDocument{\end{CJK}}
 ''',
 }
 
