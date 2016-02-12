c46ca6b6b06b4a8323633b98e21202461a8b9a3b
Lei Xu <leixu216@gmail.com>
2016-02-12 18:01:22 +0800

    rename script and add content

---
+++ scripts/azure-copy-vms.ps1
@@ -0,0 +1,121 @@
+﻿#######################################################
+# 
+# Copy VM Between Azure Environments
+# Author: Lei Xu
+# Date: 2014-10-12
+#
+####################################################### 
+
+#Parameters
+#######################################################
+$workingDir = $PSScriptRoot
+$dataDir = "d:\data"
+$CloudEnv = 'AzureChinaCloud'
+
+# Source
+$sourceSubscriptionName = 'LeiXuVSEnterprisePrepaid'
+$sourceSubscriptionSettingFile = 'LeiXuPrepaid-LeiXuVSEnterprisePrepaid-2-12-2016-credentials.publishsettings'
+$vmName = "TFS2015U1CHS"
+$sourceServiceName = "azurelab001"
+
+# Target
+$destSubscriptionName = 'LeiXuPrepaid'
+$destSubscriptionSettingFile = 'LeiXuPrepaid-LeiXuVSEnterprisePrepaid-2-12-2016-credentials.publishsettings'
+$destVNetName = 'azurelab003-vnet'
+$destStorageAccountName = 'azurelab003sa'
+$destServiceName = 'azurelab003'
+
+#######################################################
+#Remove All Subscription
+foreach ($item in  (Get-AzureSubscription))
+ {
+       Remove-AzureSubscription -SubscriptionId $item.SubscriptionId -Force
+ }
+
+#Setup Subscriptions
+####################################################### 
+#Source  
+$source=($dataDir)+"\"+($sourceSubscriptionSettingFile);
+Import-AzurePublishSettingsFile -PublishSettingsFile $source
+#Target 
+$target = ($dataDir)+"\"+($destSubscriptionSettingFile);
+Import-AzurePublishSettingsFile -PublishSettingsFile $target
+####################################################### 
+
+#Get Source VM Disks
+####################################################### 
+Select-AzureSubscription -SubscriptionName $sourceSubscriptionName
+
+#Get Source VM Data
+$sourceVm = Get-AzureVM –ServiceName $sourceServiceName –Name $vmName
+$vmConfigurationPath = $dataDir + "\$($vmName)-exportedVM.xml"
+$sourceVm | Export-AzureVM -Path $vmConfigurationPath
+$sourceOSDisk = $sourceVm.VM.OSVirtualHardDisk
+$sourceDataDisks = $sourceVm.VM.DataVirtualHardDisks
+
+$sourceStorageName = $sourceOSDisk.MediaLink.Host -split "\." | select -First 1
+$sourceStorageAccount = Get-AzureStorageAccount –StorageAccountName $sourceStorageName
+$sourceStorageKey = (Get-AzureStorageKey -StorageAccountName $sourceStorageName).Primary
+
+#Stop-AzureVM –ServiceName $sourceServiceName –Name $vmName -Force
+####################################################### 
+
+#Get Dest VM Disks
+####################################################### 
+Select-AzureSubscription -SubscriptionName $destSubscriptionName
+
+$destStorageAccount = Get-AzureStorageAccount -StorageAccountName $destStorageAccountName
+$destStorageName = $destStorageAccount.StorageAccountName
+$destStorageKey = (Get-AzureStorageKey -StorageAccountName $destStorageName).Primary
+####################################################### 
+
+#Get Source & Dest Storage Context and copy
+####################################################### 
+$sourceContext = New-AzureStorageContext –StorageAccountName $sourceStorageName -StorageAccountKey $sourceStorageKey -Environment $CloudEnv
+$destContext = New-AzureStorageContext –StorageAccountName $destStorageName -StorageAccountKey $destStorageKey -Environment $CloudEnv
+
+$allDisks = @($sourceOSDisk) + $sourceDataDisks
+$destDataDisks = @()
+foreach($disk in $allDisks)
+{
+    $blobName = $disk.MediaLink.Segments[2]
+    $targetBlob = Start-CopyAzureStorageBlob -SrcContainer vhds -SrcBlob $blobName -DestContainer vhds -DestBlob $blobName -Context $sourceContext -DestContext $destContext -Force
+    Write-host "Copying blob $blobName"
+    $copyState = $targetBlob | Get-AzureStorageBlobCopyState
+    while ($copyState.Status -ne "Success")
+    {
+        $percent = ($copyState.BytesCopied / $copyState.TotalBytes) * 100
+        Write-host "Completed $('{0:N2}' -f $percent)%"
+        sleep -Seconds 5
+        $copyState = $targetBlob | Get-AzureStorageBlobCopyState
+    }
+    If ($disk -eq $sourceOSDisk)
+    {
+        $destOSDisk = $targetBlob
+    }
+    Else
+    {
+        $destDataDisks += $targetBlob
+    }
+}
+####################################################### 
+
+#Create Target VM and Boot it up!
+####################################################### 
+Add-AzureDisk -OS $sourceOSDisk.OS -DiskName $sourceOSDisk.DiskName -MediaLocation $destOSDisk.ICloudBlob.Uri
+foreach($currenDataDisk in $destDataDisks)
+{
+    $diskName = ($sourceDataDisks | ? {$_.MediaLink.Segments[2] -eq $currenDataDisk.Name}).DiskName
+    Add-AzureDisk -DiskName $diskName -MediaLocation $currenDataDisk.ICloudBlob.Uri 
+}
+
+Get-AzureSubscription -SubscriptionName $destSubscriptionName
+Set-AzureSubscription -CurrentStorageAccountName $destStorageAccountName -SubscriptionName $destSubscriptionName 
+$vmConfig = Import-AzureVM -Path $vmConfigurationPath
+$vmConfig | Set-AzureSubnet "subnet-1" 
+
+Write-Host  "New-AzureVM..."
+New-AzureVM -ServiceName $destServiceName -VMs $vmConfig -VNetName $destVNetName 
+
+Write-Host "Completed."
+####################################################### 
---
+++ scripts/azure-create-labenv.ps1
@@ -0,0 +1,103 @@
+﻿# Get your azure scription setup correctly before running these scripts
+
+# ref: https://blogs.blackmarble.co.uk/blogs/rhepworth/post/2014/03/03/Creating-Azure-Virtual-Networks-using-Powershell-and-XML.aspx 
+
+###########################
+# Parameters
+###########################
+
+# Master Name
+$labName = "azurelab003"
+$dataDir = "d:\data"
+
+# Subscription
+$sourceSubscriptionName = 'LeiXuPrepaid'
+$sourceSubscriptionSettingFile = 'LeiXuPrepaid-LeiXuVSEnterprisePrepaid-2-12-2016-credentials.publishsettings'
+
+# Networking, Storage and Cloud Service
+$myLocation = "China North"
+$myAffinityGroup = "$($labName)-ag"
+$myVNetName = "$($labName)-vnet"
+$myVNet_Subnet1 = "subnet-1"
+$myDnsName = "$($labName)-dns"
+$myCloudServiceName = $($labName)
+$myStorageAccountName = "$($labName)sa"
+
+# Machines
+$myImageFamily = "Windows Server 2012 R2 Datacenter (zh-cn)"
+$myVmName = "TFS2015U1CHS"
+#Allowed values are 'ExtraSmall, Small, Medium, Large,ExtraLarge,A5,A6,A7,Basic_A0,Basic_A1,Basic_A2,Basic_A3,Basic_A4,Standard_D1,Standard_D2,Standard_D3,Standard_D4,Stand
+#ard_D11,Standard_D12,Standard_D13,Standard_D14,Standard_DS1,Standard_DS2,Standard_DS3,Standard_DS4,Standard_DS11,Standard_DS12
+#,Standard_DS13,Standard_DS14'.
+$myVmSize = "Small";
+
+####################################################### 
+#Setup Subscriptions
+####################################################### 
+#Source  
+$source=($dataDir)+"\"+($sourceSubscriptionSettingFile);
+Import-AzurePublishSettingsFile -PublishSettingsFile $source
+Select-AzureSubscription -SubscriptionName $sourceSubscriptionName
+
+###################################
+# Create New-AzureAffinityGroup
+###################################
+Write-Host  "Create New-AzureAffinityGroup ..."
+New-AzureAffinityGroup -Name $myAffinityGroup -Location $myLocation
+
+###################################
+# Create New VNetwork
+###################################
+Write-Host  "Create Create New VNetwork ..."
+. .\azure-vnet-lib.ps1
+ 
+$workingVnetConfig = get-azurenetworkxml
+ 
+add-azureVnetNetwork -networkName $myVNetName -affinityGroup $myAffinityGroup -addressPrefix "10.0.0.0/8"
+add-azureVnetSubnet -networkName $myVNetName -subnetName $myVNet_Subnet1 -addressPrefix "10.0.0.0/11"
+add-azureVNetDns -dnsName $myDnsName -dnsAddress "10.0.0.1"
+add-azureVnetDnsRef -networkName $myVNetName -dnsName $myDnsName
+save-azurenetworkxml($workingVnetConfig)
+
+###################################
+# Create New Cloud Service
+###################################
+#ref: https://azure.microsoft.com/en-us/documentation/articles/cloud-services-powershell-create-cloud-container/?cdn=disable
+
+Write-Host  "Create New Cloud Service ..."
+New-AzureService -ServiceName $myCloudServiceName -AffinityGroup $myAffinityGroup -Label $myCloudServiceName 
+
+############################################
+# Create New Storage account and containers
+############################################
+#ref: https://azure.microsoft.com/en-us/documentation/articles/storage-powershell-guide-full/
+
+Write-Host  "Create New Storage account and containers ..."
+New-AzureStorageAccount -StorageAccountName $myStorageAccountName -AffinityGroup $myAffinityGroup
+
+#Set default subscription and CurrentStorageAccountName
+$myDefaultSubscriptions = Get-AzureSubscription -Default
+Select-AzureSubscription -SubscriptionId $myDefaultSubscriptions.SubscriptionId -Current
+Set-AzureSubscription -SubscriptionId $myDefaultSubscriptions.SubscriptionId -CurrentStorageAccountName $myStorageAccountName
+
+#Create container for vhds
+New-AzureStorageContainer -Name "vhds"
+
+############################################
+# Create New VMs
+############################################
+# ref: https://azure.microsoft.com/en-us/documentation/articles/virtual-machines-ps-create-preconfigure-windows-vms/
+Write-Host  "Create New VMs ..."
+
+# Get all Image Names (comment this before running)
+# Get-AzureVMImage | select ImageFamily -Unique
+
+$image=Get-AzureVMImage | where { $_.ImageFamily -eq $myImageFamily } | sort PublishedDate -Descending | select -ExpandProperty ImageName -First 1
+$myVmConfig = New-AzureVMConfig -Name $myVmName -InstanceSize $myVmSize -ImageName $image
+$myVmConfig | Set-AzureSubnet -SubnetNames $myVNet_Subnet1
+$myVmCred = Get-Credential -Message "Enter username and password for the VM [$($myVmName)]"
+$myVmConfig | Add-AzureProvisioningConfig -Windows -AdminUsername $myVmCred.UserName -Password $myVmCred.GetNetworkCredential().Password
+
+New-AzureVM -ServiceName $myCloudServiceName -VMs $myVmConfig -VNetName $myVNetName
+
+Write-Host  "... Done!"
